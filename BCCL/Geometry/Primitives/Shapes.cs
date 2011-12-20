1cc2cd683b3655ef160387258decff4fceb5afb4
BinaryConstruct <binaryconstruct@gmail.com>
2011-12-20 01:22:50 -0600

    Added source of MiscUtil library and some generic primitives

---
+++ BCCL/Geometry/Primitives/Shapes.cs
@@ -0,0 +1,48 @@
+﻿using op = BCCL.Utility.GenericOperator.Operator;
+using System;
+
+namespace BCCL.Geometry.Primitives
+{
+    [Serializable]
+    public class Rectangle<T> where T : struct
+    {
+        public T X1;
+        public T X2;
+        public T Y1;
+        public T Y2;
+
+        public Rectangle(T x1, T y1, T x2, T y2)
+        {
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+        }
+
+        public T Left { get { return op.GreaterThan(X2, X1) ? X1 : X2; } }
+        public T Right { get { return op.GreaterThan(X2, X1) ? X2 : X1; } }
+        public T Top { get { return op.GreaterThan(Y2, Y1) ? Y1 : Y2; } }
+        public T Bottom { get { return op.GreaterThan(Y2, Y1) ? Y2 : Y1; } }
+        public T Height { get { return op.Subtract(Right, Left); } }
+        public T Width { get { return op.Subtract(Bottom, Top); } }
+
+        public bool Contains(T x, T y)
+        {
+            T zero = default(T);
+            T xabs = op.Subtract(x, Left);
+            T yabs = op.Subtract(y, Top);
+            if (op.LessThan(xabs, zero))
+                return false;
+
+            if (op.LessThan(yabs, zero))
+                return false;
+
+            return op.LessThan(xabs, Width) && op.LessThan(yabs, Height);
+        }
+
+        public static Rectangle<T> FromLrtb(T left, T right, T top, T bottom)
+        {
+            return new Rectangle<T>(left, top, right, bottom);
+        }
+    }
+}
--- BCCL/Geometry/Primitives/Vectors.cs
+++ BCCL/Geometry/Primitives/Vectors.cs
@@ -1,7 +1,179 @@
-﻿using System;
+using System;
 
-namespace BCCL.Geometry
+namespace BCCL.Geometry.Primitives
 {
+    #region Generics
+    [Serializable]
+    public struct Vector2<T> where T : struct
+    {
+        public T X;
+        public T Y;
+
+        public Vector2(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", X, Y);
+        }
+
+        #region Equality
+
+        public bool Equals(Vector2<T> other)
+        {
+            return other.X.Equals(X) && other.Y.Equals(Y);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (obj.GetType() != typeof(Vector2<T>)) return false;
+            return Equals((Vector2<T>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X.GetHashCode() * 397) ^ Y.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(Vector2<T> left, Vector2<T> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Vector2<T> left, Vector2<T> right)
+        {
+            return !left.Equals(right);
+        }
+
+        #endregion
+    }
+
+    [Serializable]
+    public struct Vector3<T> where T : struct
+    {
+        public T X;
+        public T Y;
+        public T Z;
+
+        public Vector3(T x, T y, T z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1},{2})", X, Y, Z);
+        }
+
+        #region Equality
+
+        public bool Equals(Vector3<T> other)
+        {
+            return other.X.Equals(X) && other.Y.Equals(Y) && other.Z.Equals(Z);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (obj.GetType() != typeof(Vector3<T>)) return false;
+            return Equals((Vector3<T>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int result = X.GetHashCode();
+                result = (result * 397) ^ Y.GetHashCode();
+                result = (result * 397) ^ Z.GetHashCode();
+                return result;
+            }
+        }
+
+        public static bool operator ==(Vector3<T> left, Vector3<T> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Vector3<T> left, Vector3<T> right)
+        {
+            return !left.Equals(right);
+        }
+        #endregion
+    }
+
+    [Serializable]
+    public struct Vector4<T> where T : struct
+    {
+        public T X;
+        public T Y;
+        public T Z;
+        public T W;
+
+        public Vector4(T x, T y, T z, T w)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1},{2},{3})", X, Y, Z, W);
+        }
+
+        #region Equality
+
+        public bool Equals(Vector4<T> other)
+        {
+            return other.X.Equals(X) && other.Y.Equals(Y) && other.Z.Equals(Z) && other.W.Equals(W);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (obj.GetType() != typeof (Vector4<T>)) return false;
+            return Equals((Vector4<T>) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int result = X.GetHashCode();
+                result = (result*397) ^ Y.GetHashCode();
+                result = (result*397) ^ Z.GetHashCode();
+                result = (result*397) ^ W.GetHashCode();
+                return result;
+            }
+        }
+
+        public static bool operator ==(Vector4<T> left, Vector4<T> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Vector4<T> left, Vector4<T> right)
+        {
+            return !left.Equals(right);
+        }
+
+        #endregion
+    }
+
+
+    #endregion
+
     #region Int32
     [Serializable]
     public struct Vector2Int32
