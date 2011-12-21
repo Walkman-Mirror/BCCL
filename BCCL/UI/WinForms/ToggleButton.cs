88d64945d2f827bec92309095fb312f97247b3f7
BinaryConstruct <binaryconstruct@gmail.com>
2011-12-21 11:50:22 -0600

    merge winforms controls

---
+++ BCCL/UI/WinForms/ToggleButton.cs
@@ -0,0 +1,251 @@
+﻿/* 
+Copyright (c) 2011 BinaryConstruct
+ 
+This source is subject to the Microsoft Public License.
+See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL.
+All other rights reserved.
+
+THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BCCL.UI.WinForms
+{
+    public class ToggleButton : Button
+    {
+        #region Events
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Category("Action")]
+        [Description("Triggers the CheckState is changed.")]
+        public event EventHandler CheckStateChanged;
+        protected virtual void OnCheckStateChanged(EventArgs e)
+        {
+            if (CheckStateChanged != null)
+                CheckStateChanged(this, e);
+
+            switch (_CheckState)
+            {
+                case CheckState.Unchecked:
+                    if (this.Checked)
+                        this.Checked = false;
+                    break;
+                case CheckState.Checked:
+                    if (!this.Checked)
+                        this.Checked = true;
+                    break;
+                case CheckState.Indeterminate:
+                    if (!this.Checked)
+                        this.Checked = true;
+                    break;
+            }
+
+        }
+
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Category("Action")]
+        [Description("Triggers the CheckState is changed.")]
+        public event EventHandler CheckedChanged;
+        protected virtual void OnCheckedChanged(EventArgs e)
+        {
+            if (CheckedChanged != null)
+                CheckedChanged(this, e);
+
+            if (_Checked)
+            {
+                if (this.CheckState != CheckState.Checked)
+                    this.CheckState = CheckState.Checked;
+            }
+            else
+            {
+                if (this.CheckState != CheckState.Unchecked)
+                    this.CheckState = CheckState.Unchecked;
+            }
+        }
+
+        #endregion
+
+        #region Properties
+        private bool _Checked = false;
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Category("Appearance")]
+        [Description("Gets or Sets the if the ToggleButton is Checked.")]
+        [DefaultValue(false)]
+        public bool Checked
+        {
+            get { return _Checked; }
+            set
+            {
+                _Checked = value;
+                OnCheckedChanged(new EventArgs());
+                SetBG();
+                this.Invalidate();
+            }
+        }
+
+        private CheckState _CheckState = CheckState.Unchecked;
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Category("Appearance")]
+        [Description("Gets or sets the CheckState of the ToggleButton")]
+        [DefaultValue(typeof(CheckState), "Unchecked")]
+        public CheckState CheckState
+        {
+            get { return _CheckState; }
+            set
+            {
+                _CheckState = value;
+                OnCheckStateChanged(new EventArgs());
+                SetBG();
+                this.Invalidate();
+            }
+        }
+
+        private bool _ThreeState = false;
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Category("Behavior")]
+        [Description("Gets or Sets the if the ToggleButton is three state.")]
+        [DefaultValue(false)]
+        public bool ThreeState
+        {
+            get { return _ThreeState; }
+            set
+            {
+                _ThreeState = value;
+                SetBG();
+                this.Invalidate();
+            }
+        }
+
+        #region Appearance
+
+        private Color _ColorChecked;
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Category("Appearance")]
+        [Description("Gets or Sets the Checked Color")]
+        [DefaultValue(typeof(Color), "Gold")]
+        public Color ColorChecked
+        {
+            get { return _ColorChecked; }
+            set { _ColorChecked = value; SetBG(); this.Invalidate(); }
+        }
+        private Color _ColorIntermediate;
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Category("Appearance")]
+        [Description("Gets or Sets the Intermediate Color")]
+        [DefaultValue(typeof(Color), "Gray")]
+        public Color ColorIntermediate
+        {
+            get { return _ColorIntermediate; }
+            set { _ColorIntermediate = value; SetBG(); this.Invalidate(); }
+        }
+
+        private Color _BackColor;
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Category("Appearance")]
+        [Description("Gets or Sets the Unchecked Color")]
+        [DefaultValue(typeof(Color), "Transparent")]
+        public override Color BackColor
+        {
+            get { return _BackColor; }
+            set { _BackColor = value; SetBG(); this.Invalidate(); }
+        }
+        #endregion
+        #endregion
+
+        public ToggleButton()
+        {
+            SetBG();
+        }
+
+
+        protected virtual void SetBG()
+        {
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            Rectangle drawArea = new Rectangle(0, 0, this.ClientRectangle.Width, this.ClientRectangle.Height);
+
+            Bitmap bg = new Bitmap(this.ClientRectangle.Width, this.ClientRectangle.Height);
+            Graphics g = Graphics.FromImage(bg);
+            g.Clear(Color.Transparent);
+            switch (this.CheckState)
+            {
+                case WinForms.CheckState.Checked:
+                    g.FillRectangle(new LinearGradientBrush(drawArea, SystemColors.ButtonHighlight, _ColorChecked, 90), drawArea);
+
+                    break;
+                case WinForms.CheckState.Indeterminate:
+                    g.FillRectangle(new LinearGradientBrush(drawArea, SystemColors.ButtonHighlight, _ColorIntermediate, 90), drawArea);
+                    break;
+                default:
+                    g.FillRectangle(new LinearGradientBrush(drawArea, SystemColors.ButtonHighlight, this.BackColor, 90), drawArea);
+                    break;
+            }
+            g.Dispose();
+
+            this.BackgroundImage = bg;
+
+            //if (this.Checked)
+            //    this.BackgroundImage = bg;
+            //else
+            //    this.BackgroundImage = null;
+
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            if (_ThreeState)
+            {
+                switch (_CheckState)
+                {
+                    case WinForms.CheckState.Unchecked:
+                        this.CheckState = WinForms.CheckState.Checked;
+                        break;
+                    case WinForms.CheckState.Checked:
+                        this.CheckState = WinForms.CheckState.Indeterminate;
+                        break;
+                    case WinForms.CheckState.Indeterminate:
+                        this.CheckState = WinForms.CheckState.Unchecked;
+                        break;
+                    default:
+                        this.CheckState = WinForms.CheckState.Unchecked;
+                        break;
+                }
+            }
+            else
+            {
+                switch (_CheckState)
+                {
+                    case WinForms.CheckState.Unchecked:
+                        this.CheckState = WinForms.CheckState.Checked;
+                        break;
+                    case WinForms.CheckState.Checked:
+                        this.CheckState = WinForms.CheckState.Unchecked;
+                        break;
+                    default:
+                        this.CheckState = WinForms.CheckState.Unchecked;
+                        break;
+                }
+            }
+
+            base.OnClick(e);
+        }
+    }
+}
