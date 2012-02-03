ad34605a94648c49f5017cd3502f6e4b60601361
BinaryConstruct <binaryconstruct@gmail.com>
2012-02-03 11:26:29 -0600

    updating text logger for multiple instancing

--- BCCL/Logging/TextLogger.cs
+++ BCCL/Logging/TextLogger.cs
@@ -19,28 +19,25 @@ namespace BCCL.Logging
 {
     public class TextLogger : ILogService
     {
-        private static string Logfile = "log.txt";
-        private static readonly string Header = "TIMESTAMP,PC,USER,SOURCE,TYPE,MSG";
+        private const string Header = "TIMESTAMP,PC,USER,SOURCE,TYPE,MSG";
         private static readonly string UserName = Environment.UserName;
         private static readonly string CompName = Environment.MachineName;
         private static readonly string Os = Environment.OSVersion.VersionString;
 
-        private static MessageLevel MinimumLogLevel;
-        private Dispatcher _dispatcher;
+        private readonly MessageLevel _minimumLogLevel;
+        private readonly string _logfile = "log.txt";
 
         private readonly ObservableCollection<string> _sessionLogs = new ObservableCollection<string>();
 
         public TextLogger()
         {
-            System.Diagnostics.Debug.WriteLine("CTOR TextLogger");
-            _dispatcher = Dispatcher.CurrentDispatcher;
-            MinimumLogLevel = MessageLevel.Info;
+            _minimumLogLevel = MessageLevel.Info;
         }
-        public TextLogger(MessageLevel level, string file)
+        public TextLogger(MessageLevel minLogLevel, string file)
             : this()
         {
-            MinimumLogLevel = level;
-            Logfile = file;
+            _minimumLogLevel = minLogLevel;
+            _logfile = file;
         }
 
         #region ILogService Members
@@ -66,35 +63,33 @@ namespace BCCL.Logging
 
         public void Log(object sender, string message, MessageLevel level)
         {
-            if (level < MinimumLogLevel)
+            if (level < _minimumLogLevel)
                 return;
 
-            _dispatcher.Invoke(
-                DispatcherPriority.Normal, new Action(
-                delegate
-                    {
-                        message = "\"" + message.Replace('\"', '\'') + "\"";
-                        string source = "\"" + sender.ToString().Replace('\"', '\'') + "\"";
-                        string line = String.Format("{0:yyyy/MM/dd HH:mm:ss},{1},{2},{3},{4},{5},{6}", DateTime.Now, CompName, Os, UserName, source, level, message);
-                        WriteLogLine(line);
-                        Messages.Insert(0, line);
+            lock (_logfile)
+            {
+                message = "\"" + message.Replace('\"', '\'') + "\"";
+                string source = "\"" + sender.ToString().Replace('\"', '\'') + "\"";
+                string line = String.Format("{0:yyyy/MM/dd HH:mm:ss},{1},{2},{3},{4},{5},{6}", DateTime.Now, CompName, Os, UserName, source, level, message);
+                WriteLogLine(line);
+                Messages.Insert(0, line);
 
-                        while (Messages.Count > 100)
-                        {
-                            Messages.RemoveAt(Messages.Count - 1);
-                        }
-                    }));
+                while (Messages.Count > 100)
+                {
+                    Messages.RemoveAt(Messages.Count - 1);
+                }
+            };
         }
 
         #endregion
 
-        private static void WriteLogLine(string line)
+        private void WriteLogLine(string line)
         {
             try
             {
-                bool exists = File.Exists(Logfile);
-                
-                using (TextWriter writer = new StreamWriter(Logfile, true))
+                bool exists = File.Exists(_logfile);
+
+                using (TextWriter writer = new StreamWriter(_logfile, true))
                 {
                     if (!exists)
                         writer.WriteLine(Header);
