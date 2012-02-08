57f52c10b9c3d641f12c8ee583ac8c90021ae400
BinaryConstruct <binaryconstruct@gmail.com>
2012-02-08 00:21:23 -0600

    list box auto scrolling

--- BCCL.Xna/Properties/AssemblyInfo.cs
+++ BCCL.Xna/Properties/AssemblyInfo.cs
@@ -53,7 +53,7 @@ using System.Windows.Markup;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.12009.2302")]
+[assembly: AssemblyFileVersion("1.0.12017.2247")]
 
 [assembly: XmlnsDefinition("http://bccl/wpf", "BCCL.UI.Xaml.XnaContentHost")]
 [assembly: XmlnsPrefix("http://bccl/wpf", "bccl")]
